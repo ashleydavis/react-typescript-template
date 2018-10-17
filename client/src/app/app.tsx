632e30b4bbff950db759e6f1db30d551fa36fcef
Ashley Davis <ashley@codecapers.com.au>
2018-10-17 16:35:39 +1000

    Updated packages. Integrated the router. Had to make an explicit dependency on @types/history to workaround broken typings. This explict dependency should later be removed.

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,4 +1,8 @@
 import * as React from 'react';
+import { HashRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
+import { HomeUI } from './home/home';
+import { Page1UI } from './page1/page1';
+import { Page2UI } from './page2/page2';
 
 export interface IAppProps {
 }
@@ -10,7 +14,19 @@ export class AppUI extends React.Component<IAppProps, IAppState> {
 
     render() {
         return (
-            <p>Your app goes here</p>
+            <HashRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <HomeUI />
+                    </Route>
+                    <Route path="/page1">
+                        <Page1UI />
+                    </Route>
+                    <Route path="/page2">
+                        <Page2UI />
+                    </Route>
+                </Switch>
+            </HashRouter>
         );
     }
 }
